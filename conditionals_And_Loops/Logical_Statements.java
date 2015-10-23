45cc2df3a4f7cb883b183917578b201e159e9628
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2015-10-23 22:00:00 +0000

    added conditionals_And_Loops\Logical_Statements.java

---
+++ conditionals_And_Loops/Logical_Statements.java
@@ -0,0 +1,19 @@
+package conditionals_And_Loops;
+
+public class Logical_Statements {
+
+	public static void main(String[] args) {
+		
+		int boy, girl;
+		boy = 8;
+		girl = 99;
+		
+		
+		if (boy > 10 ||  girl < 99){
+			System.out.println("you can enter");
+		}else{
+			System.out.println("you can NOT enter");
+		}
+	}
+
+}
