e154feeccf0356f98f85f820797751580c79d920
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2015-10-23 22:00:00 +0000

    added conditionals_And_Loops\Else_If_Statement.java

---
+++ conditionals_And_Loops/Else_If_Statement.java
@@ -0,0 +1,22 @@
+package conditionals_And_Loops;
+
+public class Else_If_Statement {
+
+	public static void main(String[] args) {
+		
+			boolean apples = true ;
+			
+			if (apples == true) { 
+				System.out.println("you have apples");
+			
+			} else if (apples == false) {
+				System.out.println("You dont have apples");
+			} else { 
+				System.out.println("i dont know what fruit you have");
+			}	
+			
+
+	}
+
+}
+                                                     
