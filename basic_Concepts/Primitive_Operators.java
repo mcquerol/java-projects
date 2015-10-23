9ab815b059a9f5878fde459dc0ca4da265a9c130
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2015-10-23 22:00:00 +0000

    added basic_Concepts\Primitive_Operators.java

---
+++ basic_Concepts/Primitive_Operators.java
@@ -0,0 +1,15 @@
+package basic_Concepts;
+
+public class Primitive_Operators {
+
+	public static void main(String[] args) {
+		int girls, boys, people;
+		girls = 11; 
+		boys = 3;
+		people =  girls % boys; // remainder of 11/3, = 2
+		
+		System.out.println(people); // output 2
+
+	}
+
+}
