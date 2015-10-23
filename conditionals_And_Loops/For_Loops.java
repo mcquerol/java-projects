cc089ecc1cb8916944e0c92a11c979a9f0906139
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2015-10-23 22:00:00 +0000

    added conditionals_And_Loops\For_Loops.java

---
+++ conditionals_And_Loops/For_Loops.java
@@ -0,0 +1,35 @@
+package conditionals_And_Loops;
+
+public class For_Loops {
+	public static void main(String[] args) {
+		
+		
+		int counter=1;
+		
+		for (/*int*/counter=1;counter <= 10; counter++) { 
+			System.out.println(counter);
+		
+			/*
+			 * you can declare
+			 * the variable inside or outside
+			 * the for loop
+			 */
+		
+		}
+		/* example::
+		 * 
+		 * for (/int/counter=2;counter<=10; counter*=)  
+		 * System.out.println(counter)
+		 * In this case it's
+		 * counter squared:
+		 *  2 * 2 = 4
+		 *  4 * 4 = 16
+		 *  16 greater than 10
+		 *  so the loop stops  
+		 */
+		
+		
+		
+		
+	}
+}
