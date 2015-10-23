f1a60536dc8a698432343157dcc09a2fccd79f85
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2015-10-23 22:00:00 +0000

    added conditionals_And_Loops\Do_While_Loops.java

---
+++ conditionals_And_Loops/Do_While_Loops.java
@@ -0,0 +1,19 @@
+package conditionals_And_Loops;
+
+public class Do_While_Loops {
+
+	public static void main(String[] args) {
+		
+		int counter = 15;
+		
+		do {
+			System.out.println(counter);
+			counter++;
+		}while(counter<=10);
+		
+		
+		
+
+	}
+
+}
