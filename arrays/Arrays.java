be7ff42846bc3fbd9ace925689a2bb336636a75a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2015-10-23 22:00:00 +0000

    added arrays\Arrays.java

---
+++ arrays/Arrays.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Arrays {
+	public static void main(String[] args) {
+		
+		int array[]={2,3,4,52,7,32,34};
+		
+	
+		
+		System.out.println(array[2]);
+		
+		
+		
+		
+		
+	}
+}
