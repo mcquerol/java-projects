7d683685501b59314198b191950ae468a3f58f75
stmacebal <mateo.cebquerol@gmail.com>
2015-10-24 23:51:20 +0200

    Adding conditionals_And_Loops\Conditional_Statements.java on 2015-10-24

---
+++ conditionals_And_Loops/Conditional_Statements.java
@@ -0,0 +1,25 @@
+package conditionals_And_Loops;
+
+public class Conditional_Statements {
+	public static void main(String[] args) {
+		
+		int test = 6;
+		
+		if (test < 9) { 				// < > <= >= == != other comparator operators
+			
+			System.out.println("Yes");
+					
+		} else { 
+				
+			System.out.println("This is else");
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+}
