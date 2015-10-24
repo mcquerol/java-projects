8453a09e189a3956d584223bf6e4f4df811bfdc1
stmacebal <mateo.cebquerol@gmail.com>
2015-10-24 23:51:17 +0200

    Adding basic_Concepts\Variables.java on 2015-10-24

---
+++ basic_Concepts/Variables.java
@@ -0,0 +1,18 @@
+package basic_Concepts;
+
+public class Variables {
+		public static void main(String[] args) {
+			
+			String name = "Mateo";
+			int age = 15;
+			double annualPay = 10212.23; // double is the same as floating point or decimal point
+			
+			System.out.println(name);		
+			System.out.println(age);
+			System.out.println(annualPay);
+			
+			// output the variables 
+			
+		}
+
+}
