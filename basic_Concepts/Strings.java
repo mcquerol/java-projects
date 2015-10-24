0a6680a05b0203844a0be68746e886c749453c86
stmacebal <mateo.cebquerol@gmail.com>
2015-10-24 23:51:16 +0200

    Adding basic_Concepts\Strings.java on 2015-10-24

---
+++ basic_Concepts/Strings.java
@@ -0,0 +1,28 @@
+package basic_Concepts;
+
+public class Strings {
+	public static void main(String[] args) {
+		
+		String firstname,middlename,lastname;
+		char firstinitial,middleinitial,lastinitial;
+		
+		
+		firstname = "Mateo";
+		middlename = "Ceballos";
+		lastname = "Querol";
+		firstinitial = 'M';
+		middleinitial = 'C';
+		lastinitial = 'Q';
+		
+		System.out.println ("My name is " + firstname + middlename + lastname);
+		
+		
+		/*
+		 *
+		 *Spaces
+		 *dont
+		 *work 
+		 * 
+		 */
+	}
+}
