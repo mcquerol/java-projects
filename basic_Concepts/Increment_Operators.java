e6b4c0535e00e9fa2fc2583d42b5c4a368791a2d
stmacebal <mateo.cebquerol@gmail.com>
2015-10-24 23:51:16 +0200

    Adding basic_Concepts\Increment_Operators.java on 2015-10-24

---
+++ basic_Concepts/Increment_Operators.java
@@ -0,0 +1,21 @@
+package basic_Concepts;
+
+public class Increment_Operators {
+		public static void main(String[] args) {
+			
+			double apple = 5; // decimal variable
+			
+			
+			apple += 8; // same as apple = apple + 8
+			System.out.println(apple);
+			
+			apple -= 8;	// same as apple = apple - 8		
+			System.out.println(apple);
+
+			apple *= 8; // same as apple = apple * 8
+			System.out.println(apple);
+			
+			
+			
+		}
+}
