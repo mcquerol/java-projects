52f73f14220439ad1313dd6aade74d6a52685176
stmacebal <mateo.cebquerol@gmail.com>
2015-10-24 23:51:22 +0200

    Adding conditionals_And_Loops\Switch_Statement.java on 2015-10-24

---
+++ conditionals_And_Loops/Switch_Statement.java
@@ -0,0 +1,43 @@
+package conditionals_And_Loops;
+
+public class Switch_Statement {
+
+	public static void main(String[] args) {
+		
+		int age;
+		age = 3;
+		
+		switch (age){
+		case 3:
+			System.out.println("You can crawl");
+		break;
+		
+		/*
+		 * you must have
+		 * as many cases
+		 * as the variable can be
+		 * instead of checking every 
+		 * value with if statements 
+		 */
+		
+		
+		default:
+			System.out.println("I dont know how old you are");
+		}
+		
+		
+		
+		
+		
+		
+		
+		/*
+		 * NOT SURE
+		 * I WILL USE
+		 * A SWITCH STATEMENT
+		 */
+		
+		
+	}
+
+}
