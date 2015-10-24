1f071f5c2762fc7f93a8fcd9bf39a6e4938e3021
stmacebal <mateo.cebquerol@gmail.com>
2015-10-24 23:51:22 +0200

    Adding conditionals_And_Loops\While_Loops.java on 2015-10-24

---
+++ conditionals_And_Loops/While_Loops.java
@@ -0,0 +1,21 @@
+package conditionals_And_Loops;
+
+public class While_Loops {
+	public static void main(String[] args) {
+		
+		int counter = 0;
+		while (counter <= 10) {
+			System.out.println(counter);
+			counter++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
