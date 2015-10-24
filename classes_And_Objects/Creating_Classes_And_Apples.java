80c3a3fff4ee9c95cb41545cf293a69c927d762a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2015-10-24 22:00:00 +0000

    added classes_And_Objects\Creating_Classes_And_Apples.java

---
+++ classes_And_Objects/Creating_Classes_And_Apples.java
@@ -0,0 +1,21 @@
+package classes_And_Objects;
+
+public class Creating_Classes_And_Apples {
+
+	public static void main(String[] args) {
+		
+		second_class secondclassObject = new second_class();
+		
+		secondclassObject.SimpleMessage();
+		// name of object    name of method
+		
+		/*
+		 * By creating another class,
+		 * you can link it to the class
+		 * with the main method
+		 * and make an object
+		 * to show what is in that other class
+		 */
+	}
+
+}
