e7c06351f8ad58f9833845ce86e486a5900e3275
stmacebal <mateo.cebquerol@gmail.com>
2015-10-24 23:51:17 +0200

    Adding basic_Concepts\User_input.java on 2015-10-24

---
+++ basic_Concepts/User_input.java
@@ -0,0 +1,14 @@
+package basic_Concepts;
+import java.util.Scanner; // the scanner is a way to check the user input 
+
+public class User_input {
+	public static void main(String[] args) {
+		
+		Scanner text = new Scanner (System.in); //variable will be declared when he scanner detects input from the keyboard
+		
+		System.out.println(text.nextLine()); //the user inputs text and the system outputs it on the next line
+		
+	}
+				
+	
+}
